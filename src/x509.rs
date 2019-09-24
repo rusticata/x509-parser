d4032d03126541a4f9a32d037d84b7f2651a9875
Jannik Schürg <schuerg@ins.uni-bonn.de>
2019-09-24 12:09:14 +0200

    Port to der-parser with as-str

--- src/x509.rs
+++ src/x509.rs
@@ -325,29 +325,29 @@ fn test_x509_name() {
             RelativeDistinguishedName{ set: vec![
                 AttributeTypeAndValue{
                     attr_type:  oid!(2.5.4.6), // countryName
-                    attr_value: DerObject::from_obj(BerObjectContent::PrintableString(b"FR")),
+                    attr_value: DerObject::from_obj(BerObjectContent::PrintableString("FR")),
                 }
             ]},
             RelativeDistinguishedName{ set: vec![
                 AttributeTypeAndValue{
                     attr_type:  oid!(2.5.4.8), // stateOrProvinceName
-                    attr_value: DerObject::from_obj(BerObjectContent::PrintableString(b"Some-State")),
+                    attr_value: DerObject::from_obj(BerObjectContent::PrintableString("Some-State")),
                 }
             ]},
             RelativeDistinguishedName{ set: vec![
                 AttributeTypeAndValue{
                     attr_type:  oid!(2.5.4.10), // organizationName
-                    attr_value: DerObject::from_obj(BerObjectContent::PrintableString(b"Internet Widgits Pty Ltd")),
+                    attr_value: DerObject::from_obj(BerObjectContent::PrintableString("Internet Widgits Pty Ltd")),
                 }
             ]},
             RelativeDistinguishedName{ set: vec![
                 AttributeTypeAndValue{
                     attr_type:  oid!(2.5.4.3), // CN
-                    attr_value: DerObject::from_obj(BerObjectContent::PrintableString(b"Test1")),
+                    attr_value: DerObject::from_obj(BerObjectContent::PrintableString("Test1")),
                 },
                 AttributeTypeAndValue{
                     attr_type:  oid!(2.5.4.3), // CN
-                    attr_value: DerObject::from_obj(BerObjectContent::PrintableString(b"Test2")),
+                    attr_value: DerObject::from_obj(BerObjectContent::PrintableString("Test2")),
                 }
             ]},
         ]
