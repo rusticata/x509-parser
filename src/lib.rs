e20e9a7b025d55a990816fc740bc4f81f48ebd33
Pierre Chifflier <chifflier@wzdftpd.net>
2020-10-21 15:47:51 +0200

    Clippy: rename lint to broken_intra_doc_links

--- src/lib.rs
+++ src/lib.rs
@@ -78,7 +78,7 @@
     unreachable_pub
 )]
 #![forbid(unsafe_code)]
-#![deny(intra_doc_link_resolution_failure)]
+#![deny(broken_intra_doc_links)]
 #![doc(test(
     no_crate_inject,
     attr(deny(warnings, rust_2018_idioms), allow(dead_code, unused_variables))
