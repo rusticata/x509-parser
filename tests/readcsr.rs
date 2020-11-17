a6bc70d9ea7154cd980d14cc12d188abadaec1b8
Pierre Chifflier <chifflier@wzdftpd.net>
2020-11-17 09:47:53 +0100

    CSR: add test for parse_x509_csr_der

---
+++ tests/readcsr.rs
@@ -0,0 +1,15 @@
+use oid_registry::OID_PKCS1_SHA256WITHRSA;
+use x509_parser::{parse_x509_csr_der, X509Version};
+
+const CSR_DATA: &[u8] = include_bytes!("../assets/csr-empty-attributes.csr");
+
+#[test]
+fn read_csr() {
+    let (rem, csr) = parse_x509_csr_der(CSR_DATA).expect("could not parse CSR");
+
+    assert!(rem.is_empty());
+    let cri = &csr.certification_request_info;
+    assert_eq!(cri.version, X509Version(0));
+    assert_eq!(cri.attributes.len(), 0);
+    assert_eq!(csr.signature_algorithm.algorithm, OID_PKCS1_SHA256WITHRSA);
+}
