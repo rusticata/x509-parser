ca2eec096824b29caec9f9cb91dcacabac2f3020
Dirkjan Ochtman <dirkjan@ochtman.nl>
2020-11-18 20:19:09 +0100

    CSR signature verification should take no arguments (fixes #66)

--- src/certification_request.rs
+++ src/certification_request.rs
@@ -69,21 +69,14 @@ impl<'a> X509CertificationRequest<'a> {
             })
     }
 
-    /// Verify the cryptographic signature of this certificate
+    /// Verify the cryptographic signature of this certification request
     ///
-    /// `public_key` is the public key of the **signer**. For a self-signed certificate,
-    /// (for ex. a public root certificate authority), this is the key from the certificate,
-    /// so you can use `None`.
-    ///
-    /// For a leaf certificate, this is the public key of the certificate that signed it.
-    /// It is usually an intermediate authority.
+    /// Uses the public key contained in the CSR, which must be the one of the entity
+    /// requesting the certification for this verification to succeed.
     #[cfg(feature = "verify")]
-    pub fn verify_signature(
-        &self,
-        public_key: Option<&SubjectPublicKeyInfo>,
-    ) -> Result<(), X509Error> {
+    pub fn verify_signature(&self) -> Result<(), X509Error> {
         use ring::signature;
-        let spki = public_key.unwrap_or(&self.certification_request_info.subject_pki);
+        let spki = &self.certification_request_info.subject_pki;
         let signature_alg = &self.signature_algorithm.algorithm;
         // identify verification algorithm
         let verification_alg: &dyn signature::VerificationAlgorithm =
