58e7c8acecc59a10b9ecf705476e63392a4fffc1
Bernd Krietenstein <bernd.krietenstein@rohde-schwarz.com>
2022-11-29 14:06:00 +0100

    new name

--- src/lib.rs
+++ src/lib.rs
@@ -128,7 +128,7 @@
     unreachable_pub
 )]
 #![forbid(unsafe_code)]
-#![deny(broken_intra_doc_links)]
+#![deny(rustdoc::broken_intra_doc_links)]
 #![doc(test(
     no_crate_inject,
     attr(deny(warnings, rust_2018_idioms), allow(dead_code, unused_variables))
