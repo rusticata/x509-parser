3b1198864fce3bbb410f16cced43c31ddd608124
Pierre Chifflier <chifflier@wzdftpd.net>
2025-04-08 16:55:35 +0200

    Migration: fix parsing for NameConstraints and add unit test

--- src/extensions/name_constraints.rs
+++ src/extensions/name_constraints.rs
@@ -16,6 +16,7 @@ pub struct NameConstraints<'a> {
     #[tag_implicit(0)]
     #[optional]
     pub permitted_subtrees: Option<Vec<GeneralSubtree<'a>>>,
+
     #[tag_implicit(1)]
     #[optional]
     pub excluded_subtrees: Option<Vec<GeneralSubtree<'a>>>,
@@ -25,6 +26,7 @@ pub struct NameConstraints<'a> {
 /// The fields minimum and maximum are not supported (openssl also has no support).
 ///
 /// <pre>
+/// -- IMPLICIT tags
 /// GeneralSubtree ::= SEQUENCE {
 ///     base                    GeneralName,
 ///     minimum         [0]     BaseDistance DEFAULT 0,
@@ -37,21 +39,34 @@ pub struct NameConstraints<'a> {
 #[error(X509Error)]
 pub struct GeneralSubtree<'a> {
     pub base: GeneralName<'a>,
-    #[tag_explicit(0)]
+
+    #[tag_implicit(0)]
     #[default(0)]
     pub minimum: u32,
-    #[tag_explicit(1)]
+
+    #[tag_implicit(1)]
     #[optional]
     pub maximum: Option<u32>,
 }
 
 #[cfg(test)]
 mod tests {
+    use asn1_rs::{DerParser, Input};
     use hex_literal::hex;
+
+    use crate::prelude::GeneralName;
+
+    use super::NameConstraints;
 
     #[test]
     fn extension_name_constraints() {
         // permitted subtree with 1 item DNS:".example.com"
         let bytes = &hex!("30 12 A0 10 30 0E 82 0C 2E 65 78 61 6D 70 6C 65 2E 63 6F 6D");
+
+        let (rem, res) = NameConstraints::parse_der(Input::from(bytes)).expect("NameConstraints");
+        assert!(rem.is_empty());
+        assert!(res.permitted_subtrees.is_some());
+        let permitted0 = &res.permitted_subtrees.as_ref().unwrap()[0];
+        assert_eq!(permitted0.base, GeneralName::DNSName(".example.com"));
     }
 }
