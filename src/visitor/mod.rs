ea3e44faa20b54c236a903dc4f980ef7ac4f08dd
Pierre Chifflier <chifflier@wzdftpd.net>
2025-05-13 14:31:22 +0200

    Add visitor pattern for Certification Request Info object and attributes (see #206)

--- src/visitor/mod.rs
+++ src/visitor/mod.rs
@@ -1,5 +1,9 @@
+//! Visitor patterns for X.509 objects
+
 mod certificate_visitor;
+mod cri_visitor;
 mod crl_visitor;
 
 pub use certificate_visitor::*;
+pub use cri_visitor::*;
 pub use crl_visitor::*;
